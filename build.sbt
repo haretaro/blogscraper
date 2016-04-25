201db1550a1ccbe4fe95a9b09e6c2a7c0d4e9af6
haretaro <frontend.1000@gmail.com>
2016-04-25 23:47:11 +0900

    とりあえず動くようになった

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ scalaVersion := "2.11.8"
 libraryDependencies ++= Seq(
   "org.seleniumhq.webdriver" % "webdriver-htmlunit" % "0.9.7376"
   )
+
+initialCommands in console := """
+import com.github.haretaro.blogscraper._
+"""
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,37 @@
+/**
+ * はてなダイヤリーから文章をたくさん引っ張ってくるプログラム
+ */
 package com.github.haretaro.blogscraper
 
 import java.io.File
-import java.net.URL
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import scala.collection.JavaConversions._
 import scala.sys.process._
 
 object Main extends App{
-  println("hello")
-  val text = scrape("http://d.hatena.ne.jp/okachimachiorz/20160424/1461498575")
-  println(text)
+  val output = "output.txt"
 
-  def scrape(url: String): String = {
+  hotEntries.map(scrapeBlog)
+    .foreach("echo %s".format(_) #>> new File(output)!)
+
+  def scrapeBlog(url: String): String = {
+    println(url)
     val driver = new HtmlUnitDriver()
     driver.get(url)
-    val content = driver.findElementByXPath("//div[@class='section']/p[2]")
-    content.getText()
+    val content = driver.findElementByXPath("//div[@class='section']")
+    Thread.sleep(100)
+    content.getText().replaceAll("Permalink.+\\Z","")
+  }
+
+  /**
+   * ホットエントリーのURLを返す
+   */
+  def hotEntries = {
+    val url = "http://b.hatena.ne.jp/hotentry/diary"
+    val driver = new HtmlUnitDriver()
+    driver.get(url)
+    val contents = driver.findElementsByXPath("//a[@class='entry-link']")
+    val urls = contents.map(_.getAttribute("href"))
+    urls.distinct
   }
 }
