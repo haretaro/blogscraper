c00702d08e0f2a6422ca69b76878d70e9d9a97ea
haretaro <frontend.1000@gmail.com>
2016-04-27 22:30:36 +0900

    ホットエントリからユーザー名を取得してそのユーザーのブログをスクレイピングするようにした

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 /**
- * はてなダイヤリーから文章をたくさん引っ張ってくるプログラム
+ * はてなダイアリーから文章をたくさん引っ張ってくるプログラム
  */
 package com.github.haretaro.blogscraper
 
@@ -11,22 +11,30 @@ import scala.sys.process._
 object Main extends App{
   val output = "output.txt"
 
-  hotEntries.map(scrapeBlog)
+  hotEntries
+    .map(wait(authorOf(_)))
+    .map(wait(allPagesOf(_)))
+    .flatten
+    .map(page => {
+      println(page)
+      wait(scrapeBlog(page))
+    })
     .foreach("echo %s".format(_) #>> new File(output)!)
 
+  /**
+   * ページのURLから本文を抜き出す
+   */
   def scrapeBlog(url: String): String = {
-    println(url)
     val driver = new HtmlUnitDriver()
     driver.get(url)
     val content = driver.findElementByXPath("//div[@class='section']")
-    Thread.sleep(100)
     content.getText().replaceAll("Permalink.+\\Z","")
   }
 
   /**
    * ホットエントリーのURLを返す
    */
-  def hotEntries = {
+  def hotEntries: Seq[String] = {
     val url = "http://b.hatena.ne.jp/hotentry/diary"
     val driver = new HtmlUnitDriver()
     driver.get(url)
@@ -38,11 +46,29 @@ object Main extends App{
   /**
    * {username} の書いたブログ記事のURLのリストを返す
    */
-  def allPagesOf(username: String) = {
+  def allPagesOf(username: String): Seq[String] = {
     val url = "http://d.hatena.ne.jp/"+username+"/archive"
     val driver = new HtmlUnitDriver()
     driver.get(url)
     val pages = driver.findElementsByXPath("//li[@class='archive archive-section']/a")
-    pages
+    pages.map(_.getAttribute("href"))
+      .filter(_.matches("http://d.hatena.ne.jp/.+"))
+  }
+
+  /**
+   * ページURLからユーザー名を抜き出す
+   */
+  def authorOf(url: String): String = {
+    val pattern = ".+d.hatena.ne.jp/(.+)/2.+\\Z".r
+    val pattern(name) = url
+    name
+  }
+
+  /**
+   * 100ms 待つ恒等写像
+   */
+  def wait[K](in:K): K = {
+    Thread.sleep(100)
+    in
   }
 }
